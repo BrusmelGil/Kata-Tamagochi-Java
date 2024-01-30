a21b2b17ddc5475ac21b69a21081b56d39972719
Brusmel Gil <Brusmel@outlook.es>
2024-01-30 17:26:56 +0100

    unfinished

--- src/main/java/com/kata/Tamagotchi.java
+++ src/main/java/com/kata/Tamagotchi.java
@@ -2,8 +2,83 @@ package com.kata;
 
 public class Tamagotchi {
     
-    int mood = 4;
+   /*  int mood = 4;
     int hunger = 4;
-    int energy = 4;
+    int energy = 4; */
+
+
+    private int hungriness;
+    private int fullness;
+    private int happiness;
+    private int tiredness;
+    private int energy;
+
+    public Tamagotchi() {
+        this.hungriness = 5;
+        this.fullness = 5;
+        this.happiness = 5;
+        this.tiredness = 3;
+        this.energy = 4;
+    }
+
+    public int getHungriness() {
+        return hungriness;
+    }
+
+    public void setHungriness(int hungriness) {
+        this.hungriness = hungriness;
+    }
+
+    public int getFullness() {
+        return fullness;
+    }
+
+    public void setFullness(int fullness) {
+        this.fullness = fullness;
+    }
+
+    public int getHappiness() {
+        return happiness;
+    }
+
+    public void setHappiness(int happiness) {
+        this.happiness = happiness;
+    }
+
+    public int getTiredness() {
+        return tiredness;
+    }
+
+    public void setTiredness(int tiredness) {
+        this.tiredness = tiredness;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+
+    public void eat() {
+        hungriness -= 2;
+        fullness += 2;
+    }
+
+    public void play() {
+        happiness += 2;
+        tiredness += 2;
+    }
+
+    public void putToBed() {
+        tiredness -= 3;
+    }
+
+    public void poop() {
+        fullness -= 2;
+    }
+
+
 
 }
--- src/test/java/com/kata/TamagotchiTest.java
+++ src/test/java/com/kata/TamagotchiTest.java
@@ -1,6 +1,7 @@
 package com.kata;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 import org.junit.jupiter.api.Test;
 
@@ -8,9 +9,41 @@ import org.junit.jupiter.api.Test;
 public class TamagotchiTest {
     
     @Test
-    public void test_example()
-    {
-        assertTrue( true );
+    public void testEat() {
+
+        Tamagotchi tamagotchi = new Tamagotchi();
+        tamagotchi.eat();
+
+        int currentHungrines = tamagotchi.getHungriness();
+
+        assertEquals(3, currentHungrines);
+    
+}
+
+@Test
+public void testPlay() {
+    Tamagotchi tamagotchi = new Tamagotchi();
+
+    tamagotchi.play();
+
+    assertEquals(7, tamagotchi.getHappiness());
+    
+}
+
+@Test
+public void testPutToBed() {
+    Tamagotchi tamagotchi = new Tamagotchi();
+
+    tamagotchi.putToBed();
+
+    assertEquals(0, tamagotchi.getTiredness());
+}
+
+@Test
+    public void testPoop() {
+        Tamagotchi tamagotchi = new Tamagotchi();
+        tamagotchi.poop();
+        assertEquals(3, tamagotchi.getFullness());
     }
 
 }
